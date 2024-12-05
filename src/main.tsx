d5960c124b06960d769ca22577c20a9dcd4ee45b
Toshit Chawda <r58playz@gmail.com>
2024-12-05 15:29:49 -0800

    nuke router

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,14 @@
 import 'dreamland/dev';
-import { Route, Router } from 'dreamland-router';
 import Home from './routes/home';
 
 import './index.css';
 import { StyleFromParams } from 'm3-dreamland';
-let router = new Router(
-	<Route>
-		<Route path="" show={<Home />} />
-	</Route>
-);
 
 const App: Component<{}, { renderRoot: HTMLElement }> = function() {
-
-	this.mount = () => {
-		router.mount(this.renderRoot);
-	}
-
 	return (
 		<div>
 			<StyleFromParams scheme="vibrant" contrast={0} color="CBA6F7" />
-			<div bind:this={use(this.renderRoot)} />
+			<Home />
 		</div>
 	)
 }
